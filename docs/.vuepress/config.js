1a1396464805347952ed65d80e58d83cb5608626
charles <820602475@qq.com>
2020-07-12 17:52:47 +0800

    111

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -128,7 +128,15 @@ function getJavaSidebar(){
     'jvm',
     'coding',
     '多线程',
-    '学习资源'
+    '学习资源',
+    {
+      title: 'Spring',
+      collapsable: true,
+      children: [
+        'spring/',
+        'spring/学习资源'
+      ]
+    }
   ]
 }
 
