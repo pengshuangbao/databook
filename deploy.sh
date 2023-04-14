4b9faae84db80005ea2cfb6d9265cad180ad09a0
charles <820602475@qq.com>
2023-04-14 17:04:31 +0800

    test
    test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pengshuangbao/databook.git master:gh-pages
+git push -f git@github.com:pengshuangbao/databook.git main:gh-pages
 
 cd -
